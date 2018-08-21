9ae7bc25629d727b073e0817ff8b85bc8e56b382
srimani-programmer <srimani.scholar@gmail.com>
2018-08-21 06:15:52 +0530

    Solution

---
+++ EasyLevel/CEILAB.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int diff;
+    diff = a - b;
+    if(diff % 10 == 0)
+        diff = diff +1;
+    else
+        diff = diff - 1;
+    cout<<diff;
+
+    return 0;
+
+}
